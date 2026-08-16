004566a11b030b37214880aac1a0dc409778c139
ishwarikadu <ishwarikadu91@gmail.com>
2026-08-16 13:08:44 +0530

    added device registration

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
-
 from database import engine, Base, get_db
 import models
-
 from auth import (
     hash_password,
     verify_password,
-    create_access_token
+    create_access_token,
+    decode_access_token
 )
-
 from schemas import (
     UserRegister,
     UserLogin,
-    TokenResponse
+    TokenResponse,
+    DeviceCreate,
+    DeviceResponse
 )
 
 Base.metadata.create_all(bind=engine)
@@ -112,3 +113,67 @@ def login(
         "access_token": token,
         "token_type": "bearer"
     }
+security = HTTPBearer()
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+):
+    token = credentials.credentials
+
+    user_id = decode_access_token(token)
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+    user = (
+        db.query(models.User)
+        .filter(models.User.id == int(user_id))
+        .first()
+    )
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+
+    return user
+@app.post(
+    "/devices",
+    response_model=DeviceResponse
+)
+def register_device(
+    device: DeviceCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)
+):
+    existing_device = (
+        db.query(models.Device)
+        .filter(models.Device.device_id == device.device_id)
+        .first()
+    )
+
+    if existing_device:
+        raise HTTPException(
+            status_code=400,
+            detail="Device already registered"
+        )
+
+    new_device = models.Device(
+        device_id=device.device_id,
+        device_type=device.device_type,
+        model=device.model,
+        storage=device.storage,
+        status="READY_TO_SANITIZE",
+        user_id=current_user.id
+    )
+
+    db.add(new_device)
+    db.commit()
+    db.refresh(new_device)
+
+    return new_device
